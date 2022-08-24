174fcc2256a734aab4fef6990b55be886f4270a9
Jose Sebastián Solezzi <80178385+sebsolezzi@users.noreply.github.com>
2022-08-23 21:49:41 -0300

    23/08/22-21:49

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors';
 import { sequelize } from './config/db.js';
 import usersRoutes from './routes/users.routes.js';
 import taskRoutes from './routes/task.routes.js';
@@ -6,6 +7,7 @@ import taskRoutes from './routes/task.routes.js';
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/users',usersRoutes);
 app.use('/tasks',taskRoutes);
